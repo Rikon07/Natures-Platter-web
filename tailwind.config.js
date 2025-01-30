028138b24d63eab7f71c409eea69d38abbe37882
Rikon07 <mh13mahedi@gmail.com>
2025-01-30 22:54:15 +0600

    Completed the Banner section :p

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'white': '#FFF',
+          'light-beige': '#EFEBE3',
+        },
+      },
+    },
+    plugins: [
+      require('tailwindcss-gradients'),
+    ],
+  }
